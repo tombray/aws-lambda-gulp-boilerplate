64799c85972ded3e1554e1a9ccc9b1a546acbbb3
tombray <tombray@gmail.com>
2016-01-04 22:48:31 -0800

    refactor complete

--- gulpfile.js
+++ gulpfile.js
@@ -11,94 +11,69 @@ var prompt = require('gulp-prompt');
 var fs = require('fs'),
     path = require('path');
 
+AWS.config.region = 'us-west-2';
+var lambda = new AWS.Lambda({apiVersion: '2015-03-31'});    
+
 var handleError = function (msg) {
   gutil.log(gutil.colors.red('ERROR!', msg)) ;
   process.exit(1);
 }
 
 var argv = require('yargs')
-.command('update', 'Updates an AWS Lambda, assuming it already exists.', function (yargs, argv) {
-  argv = yargs.option('l', {
+.command('info', 'get information about a currently deployed lambda')
+.command('clean', 'delete the dist folder in the lambda subdirectory')
+.command('js', 'Copy any javascript files into dist.')
+.command('zip', 'Zip up the dist folder.')
+.command('update', 'Updates an AWS Lambda, assuming it already exists.')
+.option('l', {
         alias: 'lambda',
         description: 'Name of the directory that contains your function. Must be a subdirectory of ./lambda and must match the AWS function name.',
         demand: true
       })
-      .argv  
-})
 .argv;
 
-var validateLambda = function () {
-  var lambda = argv.lambda;
-
-  if (lambda) {
-    var lambdaPath = 'lambda/' + lambda;
-    //check that directory exists
-    try {
-      fs.statSync(lambdaPath);  
-    } catch(e) {
-      handleError(lambdaPath + ' does not exist');
-    }
-  }  
+var lambdaName = argv.lambda;
+var lambdaPath = 'lambda/' + lambdaName;
+var dist = lambdaPath + '/dist';
+  
+//check that directory exists
+try {
+  fs.statSync(lambdaPath);  
+} catch(e) {
+  handleError(lambdaPath + ' does not exist');
 }
 
+//BEGIN TASKS
 
-/*
-var lambdaName = argv.l;
-var lambdaDir = "./lambda/" + lambdaName;
-var DIST = lambdaDir + "/dist";
-*/
-
-// First we need to clean out the dist folder and remove the compiled zip file.
 gulp.task('clean', function(done) {
-  del(DIST).then(function(){
+  del(dist).then(function(){
    done()
   });
 });
 
 gulp.task('js', function() {
-  return gulp.src( lambdaDir + '/*.js')
-    .pipe(gulp.dest(DIST));
+  return gulp.src( lambdaPath + '/*.js')
+    .pipe(gulp.dest(dist));
 });
 
 // Here we want to install npm packages to dist, ignoring devDependencies.
 gulp.task('npm', function() {
-  return gulp.src( lambdaDir + '/package.json')
-    .pipe(gulp.dest(DIST))
+  return gulp.src( lambdaPath + '/package.json')
+    .pipe(gulp.dest(dist))
     .pipe(install({production: true}));
 });
 
-// Next copy over environment variables managed outside of source control.
-gulp.task('env', function() {
-  return gulp.src( lambdaDir + '/config.env.production')
-    .pipe(rename('.env'))
-    .pipe(gulp.dest(DIST));
-});
-
-// Now the dist directory is ready to go. Zip it.
 gulp.task('zip', function() {
-  return gulp.src([ DIST + '/**/*', '!' + DIST + '/package.json', DIST + '/.*'])
+  return gulp.src([ dist + '/**/*', '!' + dist + '/package.json', dist + '/.*'])
     .pipe(zip('dist.zip'))
-    .pipe(gulp.dest(lambdaDir));
+    .pipe(gulp.dest(dist));
 });
 
-// Per the gulp guidelines, we do not need a plugin for something that can be
-// done easily with an existing node module. #CodeOverConfig
-//
-// Note: This presumes that AWS.config already has credentials. This will be
-// the case if you have installed and configured the AWS CLI.
-//
-// See http://aws.amazon.com/sdk-for-node-js/
-gulp.task('xupload', function(done) {
-
-  // TODO: This should probably pull from package.json
-  AWS.config.region = 'us-west-2';
-  var lambda = new AWS.Lambda({apiVersion: '2015-03-31'});
-  var functionName = lambdaName;
-
-  lambda.getFunction({FunctionName: functionName}, function(err, data) {
+gulp.task('info', function (done) {
+  lambda.getFunction({FunctionName: lambdaName}, function(err, data) {
     if (err) {
       if (err.statusCode === 404) {
-        var warning = 'Unable to find lambda function ' + deploy_function + '. '
+        var warning = 'Unable to find lambda function ' + lambdaName + '. '
         warning += 'Verify the lambda function name and AWS region are correct.'
         gutil.log(warning);
       } else {
@@ -107,24 +82,32 @@ gulp.task('xupload', function(done) {
         gutil.log(warning);
       }
     }
+    gutil.log(data.Configuration);
+    done();
+  });  
+
+})
+
+gulp.task('update', function (done) {
+
+  var functionName = lambdaName;
 
     var params = {
       FunctionName: functionName,
       Publish: true
     };
 
-    fs.readFile( lambdaDir + '/dist.zip', function(err, data) {
+    fs.readFile( dist + '/dist.zip', function(err, data) {
+      if (err) handleError(err);
+
       params['ZipFile'] = data;
       lambda.updateFunctionCode(params, function(err, data) {
-        if (err) {
-          console.log(err);
-          var warning = 'Package upload failed.';
-          gutil.log(warning);
-          done()
-        }
+        if (err) handleError(err);
+        gutil.log(gutil.colors.green('Successfully updated'), gutil.colors.cyan(lambdaName));
+        gutil.log(data);
       });
     });
-  });
+
 });
 
 // The key to deploying as a single command is to manage the sequence of events.
@@ -133,17 +116,9 @@ gulp.task('default', function(callback) {
     'clean',
     ['js', 'npm'],
     'zip',
-    'upload',
+    'update',
     callback
   );
-});
-
-
-
-gulp.task('update', function() {
-  console.log('update called');
-  validateLambda();
-
 });
 
 
