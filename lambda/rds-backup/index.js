d52e5bbd62a6e4660df04ce74bb7b9aea905f87f
Michael Arnold <marnold@cj.com>
2017-07-21 13:53:29 -0700

    only copy snapshots from instances tagged with {backup: true}

--- lambda/rds-backup/index.js
+++ lambda/rds-backup/index.js
@@ -31,20 +31,60 @@ var copyDBSnapshots = function(allSnapshots) {
 };
 
 // =============LOGIC===============
-AWS.config.update({region: 'us-east-1'});
-
 console.log('Loading function...');
 
 exports.handler = function(event, context) {
-  var params = {
-    DBInstanceIdentifier: event.DBInstanceIdentifier
-  };
+  AWS.config.update({region: event.region});
 
-  console.log('Describing DB Snapshots...');
-  rds.describeDBSnapshots(params, function(err, data) {
+  console.log('Getting all DB instances...');
+  rds.describeDBInstances({}, function(err, data) {
     if (err) console.log(err, err.stack);
     else {
-        copyDBSnapshots(data.DBSnapshots);
+
+      if (!data.DBInstances) {
+        console.log('No DB instances available; exiting.');
+        return;
+      } else {
+        console.log('Instances found: ' + data.DBInstances.map(i => i.DBInstanceIdentifier));
+      }
+
+      // map over db instances and grab tags for each
+      data.DBInstances.map(function(DBInstance) {
+        var dbId = DBInstance.DBInstanceIdentifier;
+
+        console.log('Getting tags for id: ' + dbId);
+
+        var params = {
+          ResourceName: DBInstance.DBInstanceArn
+        };
+        rds.listTagsForResource(params, function(err, data) {
+          if (err) console.log(err, err.stack);
+          else {
+            // if tags contain backup tag, copy snapshots for this db
+            if (!data.TagList) {
+              console.log('No tags found; skipping id: ' + dbId);
+              return;
+            }
+            var backupTag = data.TagList.filter (tag => tag === {Key: 'backup', Value: 'true'});
+            if (backupTag.length === 0) {
+              console.log('This instance is not tagged with {backup: true}; skipping id: ' + dbId);
+              return;
+            }
+            var params = {
+              DBInstanceIdentifier: dbId
+            }
+            rds.describeDBSnapshots(params, function(err, data) {
+              var params = {
+                DBInstanceIdentifier:  dbId
+              };
+              if (err) console.log(err, err.stack);
+              else {
+                copyDBSnapshots(data.DBSnapshots);
+              }
+            });
+          }
+        });
+      })
     }
   });
 };
